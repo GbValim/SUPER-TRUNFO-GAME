f860a71af12367dc7af31dcb110c47764e8328b5
GbValim <gabriel07mercury@gmail.com>
2026-09-24 20:14:30 -0300

    limpeza

--- vetores/aulaFor.py
+++
@@ -1,5 +0,0 @@
-#forech >>>> for de busca
-for i in range(0,101):
-    print(i)
-
-    
--- vetores/exeFor.py
+++
@@ -1,9 +0,0 @@
-#Contrua um programa onde o usuario digitará
-#um numero e o programa completará o numero
-#digitando até 0, apenas com numeros pares
-
-numero = int(input("Digite um numero:  \n"))
-
-for i in range(0, 11):
-    print("\n A tabuada desse numero é: \N",  numero * i)
-    
