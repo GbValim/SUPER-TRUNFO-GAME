42f73cb1325c40f2d7cd7a2a5ab68bda761e59b3
GbValim <gabriel07mercury@gmail.com>
2026-09-24 19:51:05 -0300

    organizando atividades

---
+++ vetores/exe-lista03.py
@@ -0,0 +1,6 @@
+numeros = []
+for i in range(0,5):
+     numero = float(input("Digite um numero")) 
+     numeros.append(numero.sort()) 
+
+    
---
+++ vetores/exeFor.py
@@ -0,0 +1,9 @@
+#Contrua um programa onde o usuario digitará
+#um numero e o programa completará o numero
+#digitando até 0, apenas com numeros pares
+
+numero = int(input("Digite um numero:  \n"))
+
+for i in range(0, 11):
+    print("\n A tabuada desse numero é: \N",  numero * i)
+    
